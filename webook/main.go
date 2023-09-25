4278b208e75453dc5c10afb3bec4bc67b77e78f4
Deng Ming <ming_flycash@qq.com>
2023-09-25 16:26:27 +0800

    多实例 Session 问题

--- webook/main.go
+++ webook/main.go
@@ -8,7 +8,7 @@ import (
 	"gitee.com/geekbang/basic-go/webook/internal/web/middleware"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -71,7 +71,17 @@ func initWebServer() *gin.Engine {
 	login := &middleware.LoginMiddlewareBuilder{}
 	// 存储数据的，也就是你 userId 存哪里
 	// 直接存 cookie
-	store := cookie.NewStore([]byte("secret"))
+	//store := cookie.NewStore([]byte("secret"))
+	// 基于内存的实现
+	//store := memstore.NewStore([]byte("k6CswdUm75WKcbM68UQUuxVsHSpTCwgK"),
+	//	[]byte("eF1`yQ9>yT1`tH1,sJ0.zD8;mZ9~nC6("))
+	store, err := redis.NewStore(16, "tcp",
+		"localhost:6379", "",
+		[]byte("k6CswdUm75WKcbM68UQUuxVsHSpTCwgK"),
+		[]byte("k6CswdUm75WKcbM68UQUuxVsHSpTCwgA"))
+	if err != nil {
+		panic(err)
+	}
 	server.Use(sessions.Sessions("ssid", store), login.CheckLogin())
 	return server
 }
