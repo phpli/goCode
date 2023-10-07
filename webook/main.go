155adb81fe6c5011619f33d11843d6121db19b89
Deng Ming <ming_flycash@qq.com>
2023-10-07 14:54:05 +0800

    wire 改造代码

--- webook/main.go
+++ webook/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"gitee.com/geekbang/basic-go/webook/config"
 	"gitee.com/geekbang/basic-go/webook/internal/repository"
 	"gitee.com/geekbang/basic-go/webook/internal/repository/cache"
 	"gitee.com/geekbang/basic-go/webook/internal/repository/dao"
@@ -10,27 +9,16 @@ import (
 	"gitee.com/geekbang/basic-go/webook/internal/service/sms/localsms"
 	"gitee.com/geekbang/basic-go/webook/internal/web"
 	"gitee.com/geekbang/basic-go/webook/internal/web/middleware"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"net/http"
-	"strings"
-	"time"
 )
 
 func main() {
-	db := initDB()
-	redisClient := redis.NewClient(&redis.Options{
-		Addr: config.Config.Redis.Addr,
-	})
-	server := initWebServer()
-	codeSvc := initCodeSvc(redisClient)
-	initUserHdl(db, redisClient, codeSvc, server)
-	//server := gin.Default()
+	server := InitWebServer()
 	server.GET("/hello", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "hello，启动成功了！")
 	})
@@ -59,47 +47,9 @@ func initMemorySms() sms.Service {
 	return localsms.NewService()
 }
 
-func initDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
-	if err != nil {
-		panic(err)
-	}
-
-	err = dao.InitTables(db)
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
 func initWebServer() *gin.Engine {
 	server := gin.Default()
 
-	server.Use(cors.New(cors.Config{
-		//AllowAllOrigins: true,
-		//AllowOrigins:     []string{"http://localhost:3000"},
-		AllowCredentials: true,
-
-		AllowHeaders: []string{"Content-Type", "Authorization"},
-		// 这个是允许前端访问你的后端响应中带的头部
-		ExposeHeaders: []string{"x-jwt-token"},
-		//AllowHeaders: []string{"content-type"},
-		//AllowMethods: []string{"POST"},
-		AllowOriginFunc: func(origin string) bool {
-			if strings.HasPrefix(origin, "http://localhost") {
-				//if strings.Contains(origin, "localhost") {
-				return true
-			}
-			return strings.Contains(origin, "your_company.com")
-		},
-		MaxAge: 12 * time.Hour,
-	}), func(ctx *gin.Context) {
-		println("这是我的 Middleware")
-	})
-
-	//redisClient := redis.NewClient(&redis.Options{
-	//	Addr: config.Config.Redis.Addr,
-	//})
 	//
 	//server.Use(ratelimit.NewBuilder(redisClient,
 	//	time.Second, 1).Build())
