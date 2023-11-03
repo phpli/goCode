eea65283f036ace558bf4307f08bf816159ec832
Deng Ming <ming_flycash@qq.com>
2023-11-03 11:47:16 +0800

    状态维护

--- webook/internal/repository/article.go
+++ webook/internal/repository/article.go
@@ -11,6 +11,7 @@ type ArticleRepository interface {
 	Create(ctx context.Context, art domain.Article) (int64, error)
 	Update(ctx context.Context, art domain.Article) error
 	Sync(ctx context.Context, art domain.Article) (int64, error)
+	SyncStatus(ctx context.Context, uid int64, id int64, status domain.ArticleStatus) error
 }
 
 type CachedArticleRepository struct {
@@ -20,6 +21,10 @@ type CachedArticleRepository struct {
 	authorDAO dao.ArticleAuthorDAO
 
 	db *gorm.DB
+}
+
+func (c *CachedArticleRepository) SyncStatus(ctx context.Context, uid int64, id int64, status domain.ArticleStatus) error {
+	return c.dao.SyncStatus(ctx, uid, id, status.ToUint8())
 }
 
 func (c *CachedArticleRepository) Sync(ctx context.Context, art domain.Article) (int64, error) {
@@ -108,5 +113,7 @@ func (c *CachedArticleRepository) toEntity(art domain.Article) dao.Article {
 		Title:    art.Title,
 		Content:  art.Content,
 		AuthorId: art.Author.Id,
+		//Status:   uint8(art.Status),
+		Status: art.Status.ToUint8(),
 	}
 }
