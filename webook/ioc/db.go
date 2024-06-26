a700d2c9738fb10dcbf2a76a4a8e599db98079ff
changdong <lichangdong100@gmail.com>
2024-06-26 10:38:26 +0800

    6.25 gorm 日志

--- webook/ioc/db.go
+++ webook/ioc/db.go
@@ -2,12 +2,15 @@ package ioc
 
 import (
 	"gitee.com/geekbang/basic-go/webook/internal/repository/dao"
+	logger2 "gitee.com/geekbang/basic-go/webook/pkg/logger"
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	glogger "gorm.io/gorm/logger"
+	"time"
 )
 
-func InitDB() *gorm.DB {
+func InitDB(l logger2.LoggerV1) *gorm.DB {
 	//dsn := viper.GetString("db.mysql.dsn")
 	type Config struct {
 		DSN string `yaml:"dsn"`
@@ -21,7 +24,15 @@ func InitDB() *gorm.DB {
 		panic(err)
 	}
 	//db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
-	db, err := gorm.Open(mysql.Open(cfg.DSN))
+	db, err := gorm.Open(mysql.Open(cfg.DSN), &gorm.Config{
+		Logger: glogger.New(gormLoggerFunc(l.Debug), glogger.Config{
+			//慢查询50ms，100ms. 一般 一次的磁盘io是10ms，给50ms 一般就是 一句给了5次
+			SlowThreshold:             time.Millisecond * 50, //慢查询日志的阈值
+			IgnoreRecordNotFoundError: true,                  //dev 环境设置为 false 比较好，正式环境设置为true
+			LogLevel:                  glogger.Info,
+			ParameterizedQueries:      true,
+		}),
+	})
 	if err != nil {
 		panic(err)
 	}
@@ -30,4 +41,10 @@ func InitDB() *gorm.DB {
 		panic(err)
 	}
 	return db
+}
+
+type gormLoggerFunc func(msg string, fields ...logger2.Field)
+
+func (g gormLoggerFunc) Printf(msg string, args ...interface{}) {
+	g(msg, logger2.Field{Key: "args", Value: args})
 }
