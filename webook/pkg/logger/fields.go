e86c2afb37f6b461c2ccddcd8285e67c9d15c5b6
changdong <lichangdong100@gmail.com>
2024-07-02 15:15:53 +0800

    7.1  作业 同步转异步

--- webook/pkg/logger/fields.go
+++ webook/pkg/logger/fields.go
@@ -7,3 +7,17 @@ func String(key string, value any) Field {
 func Error(err error) Field {
 	return Field{Key: "error", Value: err}
 }
+func Bool(key string, val bool) Field {
+	return Field{Key: key, Value: val}
+}
+func Int32(key string, val int32) Field {
+	return Field{Key: key, Value: val}
+}
+
+func Int64(key string, val int64) Field {
+	return Field{Key: key, Value: val}
+}
+
+func Int(key string, val int) Field {
+	return Field{Key: key, Value: val}
+}
